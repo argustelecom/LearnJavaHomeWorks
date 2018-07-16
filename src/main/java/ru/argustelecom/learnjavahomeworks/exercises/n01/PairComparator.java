7aa98bbc475f5ee2f82fc011c2a79527c67a1f60
mike <mike.argus@yandex.ru>
2018-07-16 17:47:15 +0300

    Add files via upload
    Задание #1

---
+++ src/main/java/ru/argustelecom/learnjavahomeworks/exercises/n01/PairComparator.java
@@ -0,0 +1,17 @@
+import java.util.Comparator;
+
+public class PairComparator<T extends Pair<? extends Integer,?>> implements Comparator<T> {
+	/* Как бы здесь уйти от явного extends Integer ? 
+	 * Если использовать Comparable<?>, компилятор не даёт использовать compareTo:
+	 * 
+	 * public class PairComparator<T extends Pair<Comparable<?>, ?>> implements Comparator<T>
+	 * ...
+	 * return p1.getFirst().compareTo(p2.getFirst());
+	 * 
+	 * >> The method compareTo(capture#2-of ?) in the type Comparable<capture#2-of ?> 
+	 *     is not applicable for the arguments (Comparable<capture#4-of ?>)
+	 */
+	public int compare(T p1, T p2) {
+		return p1.getFirst().compareTo(p2.getFirst());
+  }
+}
