9104e935283dedd16bc14043fd9c9eed00a3d712
SemchVlad <semchenko10@gmail.com>
2018-08-06 10:23:26 +0300

    удаляю чужую реализацию, так как тут буду пробовать собирать свою реализацию.
    ветка только для задания 2.

--- src/main/java/ru/argustelecom/learnjavahomeworks/exercises/n01/model/package-info.java
+++
@@ -1,4 +0,0 @@
-/*
-    Модель для первого упражнения
- */
-package ru.argustelecom.learnjavahomeworks.exercises.n01.model;
--- src/main/java/ru/argustelecom/learnjavahomeworks/exercises/n02/vtr/Main.java
+++
@@ -1,9 +0,0 @@
-package ru.argustelecom.learnjavahomeworks.exercises.n02.vtr;
-
-import javax.persistence.EntityManager;
-
-public class Main {
-    public static void main(String[] args) {
-        ;
-    }
-}
